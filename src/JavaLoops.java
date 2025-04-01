e65832550761ffeac3ac49d6f3841a143512ccc9
Nam <stghoainam4002@gmail.com>
2025-04-01 19:51:57 +0700

    Updated

--- src/JavaLoops.java
+++ src/JavaLoops.java
@@ -4,8 +4,19 @@ public class JavaLoops {
             System.out.println(i);
         }
     }
+
+    void vongLapForEach() {
+        int[] arr = {1, 3, 5};
+        for(int item : arr) {
+            System.out.println(item);
+        }
+    }
+
     public static void main(String[] args) {
         JavaLoops javaLoops = new JavaLoops();
+        System.out.println("Vong lap for");
         javaLoops.vongLapFor();
+        System.out.println("Vong lap for-each");
+        javaLoops.vongLapForEach();
     }
 }
